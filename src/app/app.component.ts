6d40fdc6007de91a93adce4b72e286e2dfa779dd
vaibhavinagarkar <vaibhavin24@.com>
2021-06-22 22:51:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'directives';
+  
+  name = 'Felix';
+  lastName='nagarkar'
+  email = 'felix@gmail.com'
+  password='fmjfgdd@123'
+  contact = '1234567890'
+  address= 'aswhfsmfe'
+  address2= 'jsnwd23nd'
+  city='pune'
+  state='maharastra'
+  zip='425001'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
